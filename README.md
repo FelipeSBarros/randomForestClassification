fd0368f36c5dc02a1e40c91bc21badeeb0d38770
Felipe Barros <felipe.b4rros@gmail.com>
2019-03-03 16:36:16 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Forest classification with R
+Repositório com script ensinando a usar o modelo randomForest para classificação de imagem de satélites.
+![youtube](youtube.com)
---
+++ script_randomForest.R
@@ -0,0 +1,44 @@
+# instalando as bibliotecas:
+# install.packages("randomForest")
+# install.packages("sf")
+# install.packages("tidyverse")
+# install.packages("raster")
+library(randomForest)
+library(sf)
+library(tidyverse)
+library(raster)
+
+# Carregando os dados: ----
+# raster
+img <- stack(list.files("./LC08_L1TP_220068_20180906_20180912_01_T1/Reflectancia/", pattern = ".TIF$", full.names = T)[3:6])
+
+plotRGB(img, 4, 3, 2, stretch = 'hist')
+names(img) <- paste0(rep('band', nlayers(img)), 1:nlayers(img))
+
+# amostras
+amostras <- read_sf("./Amostras/Amostras.shp")
+plot(amostras, add=T)
+
+# Extreção de dados:
+valsTrain <- raster::extract(img, amostras)
+head(valsTrain)
+
+valsTrain <- data.frame(valsTrain, amostras$class)
+head(valsTrain)
+names(valsTrain)[ncol(valsTrain)] <- "class"
+
+valsTrain$class <- as.factor(valsTrain$class)
+class(valsTrain$class)
+# Criando modelo randomForest
+rf.mdl <- randomForest(valsTrain$class ~., data = valsTrain)
+rf.mdl
+
+getTree(rf.mdl, k=1)
+
+varImpPlot(rf.mdl)
+
+# Classificação ----
+rf.class <- raster::predict(img, rf.mdl, progress = "text", type = "response")
+
+# Salvando classificação: ----
+writeRaster(rf.class, "rf_Classification.tif", overwrite = TRUE)
